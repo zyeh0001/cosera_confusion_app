3182223fee3870e1b2f40fe580f490b740dd6bcb
zyeh0001 <69131379+zyeh0001@users.noreply.github.com>
2021-12-29 13:52:49 +1100

    uncontroll form

--- src/component/ContactComponent.js
+++ src/component/ContactComponent.js
@@ -8,6 +8,7 @@ import {
   Label,
   Input,
   Col,
+  FormFeedback,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
@@ -23,9 +24,17 @@ class Contact extends Component {
       agree: false,
       contactType: 'Tel',
       message: '',
+      touched: {
+        firstname: false,
+        lastname: false,
+        email: false,
+        telnum: false,
+      }, //validation of the form set to true if violate
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.validate = this.validate.bind(this);
   }
 
   handleInputChange(event) {
@@ -42,7 +51,48 @@ class Contact extends Component {
     event.preventDefault();
   }
 
+  handleBlur = (field) => (evt) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+    });
+  };
+  validate(firstname, lastname, telnum, email) {
+    const error = {
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+    };
+    if (this.state.touched.firstname && firstname.length < 3)
+      error.firstname = 'First Name should be >= 3 charaters';
+    else if (this.state.touched.firstname && firstname.length > 10)
+      error.firstname = 'First Name should be between 3 to 10 charaters';
+
+    if (this.state.touched.lastname && lastname.length < 3)
+      error.lastname = 'Last Name should be >= 3 charaters';
+    else if (this.state.touched.lastname && lastname.length > 10)
+      error.lastname = 'Last Name should be between 3 to 10 charaters';
+
+    const reg = /^\d+$/;
+    if (this.state.touched.telnum && !reg.test(telnum))
+      error.telnum = 'Tel. Number should contain only numbers';
+
+    if (
+      this.state.touched.email &&
+      email.split('').filter((x) => x === '@').length !== 1
+    )
+      error.email = 'Email should contain a @';
+
+    return error;
+  }
+
   render() {
+    const errors = this.validate(
+      this.state.firstname,
+      this.state.lastname,
+      this.state.telnum,
+      this.state.email
+    );
     return (
       <div className='container'>
         <div className='row'>
@@ -120,8 +170,12 @@ class Contact extends Component {
                     name='firstname'
                     placeholder='First Name'
                     value={this.state.firstname}
+                    valid={errors.firstname === ''}
+                    invalid={errors.firstname !== ''}
+                    onBlur={this.handleBlur('firstname')}
                     onChange={this.handleInputChange}
                   />
+                  <FormFeedback>{errors.firstname}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -135,8 +189,12 @@ class Contact extends Component {
                     name='lastname'
                     placeholder='Last Name'
                     value={this.state.lastname}
+                    valid={errors.lastname === ''}
+                    invalid={errors.lastname !== ''}
+                    onBlur={this.handleBlur('lastname')}
                     onChange={this.handleInputChange}
                   />
+                  <FormFeedback>{errors.lastname}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -150,8 +208,12 @@ class Contact extends Component {
                     name='telnum'
                     placeholder='Tel. number'
                     value={this.state.telnum}
+                    valid={errors.telnum === ''}
+                    invalid={errors.telnum !== ''}
+                    onBlur={this.handleBlur('telnum')}
                     onChange={this.handleInputChange}
                   />
+                  <FormFeedback>{errors.telnum}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -165,8 +227,12 @@ class Contact extends Component {
                     name='email'
                     placeholder='Email'
                     value={this.state.email}
+                    valid={errors.email === ''}
+                    invalid={errors.email !== ''}
+                    onBlur={this.handleBlur('email')}
                     onChange={this.handleInputChange}
                   />
+                  <FormFeedback>{errors.email}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
--- src/component/Header.js
+++ src/component/Header.js
@@ -6,6 +6,14 @@ import {
   NavbarToggler,
   Collapse,
   NavItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  Label,
+  Form,
+  Input,
 } from 'reactstrap';
 import { Jumbotron } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
@@ -16,13 +24,35 @@ class Header extends Component {
     super(props);
     this.state = {
       isNavOpen: false,
+      isModalOpen: false,
     };
     this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
   toggleNav() {
     this.setState({
       isNavOpen: !this.state.isNavOpen,
     });
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  handleLogin(event) {
+    this.toggleModal();
+    alert(
+      'Username:' +
+        this.username.value +
+        'Password: ' +
+        this.password.value +
+        'Remember Me: ' +
+        this.remember.checked
+    );
+    event.preventDefault();
   }
   render() {
     return (
@@ -62,6 +92,13 @@ class Header extends Component {
                   </NavLink>
                 </NavItem>
               </Nav>
+              <Nav className='mlauto' navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className='fa fa-sign-in fa-lg'></span>Login
+                  </Button>
+                </NavItem>
+              </Nav>
             </Collapse>
           </div>
         </Navbar>
@@ -79,6 +116,44 @@ class Header extends Component {
             </div>
           </div>
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor='username'>Username</Label>
+                <Input
+                  type='text'
+                  id=' username'
+                  name='username'
+                  innerRef={(input) => (this.username = input)}
+                ></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor='password'>Password</Label>
+                <Input
+                  type='password'
+                  id=' password'
+                  name='password'
+                  innerRef={(input) => (this.password = input)}
+                ></Input>
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type='checkbox'
+                    name='remember'
+                    innerRef={(input) => (this.remember = input)}
+                  />
+                  Remember me
+                </Label>
+              </FormGroup>
+              <Button type='submit' value='submit' className='bg-primary'>
+                Login
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
       </>
     );
   }
